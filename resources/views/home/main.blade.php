7933a14728d128f3e07bb1c50039962463f95819
AndresG <gazuiandres@gmail.com>
2020-07-10 15:20:45 -0400

    modal, nav, footer, cms

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -8,5 +8,10 @@
 	@include('home.menu')
 	@yield('content')
 	@include('common.footer')
+	{{-- @include('modales.modal_contacto') --}}
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="js/materialize.js"></script>
+	<script src="js/init.js"></script>
+	<script src='vendor/aos/aos.js'></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/cms', function () {
+	return view('cms.index');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
