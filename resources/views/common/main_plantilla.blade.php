89fb7fbba3276d6926661ad70fcde42f66e2f449
Carlos Maita <carlosmaita2009@gmail.com>
2020-09-07 02:02:33 -0400

    ajustes

--- resources/views/common/main_plantilla.blade.php
+++ resources/views/common/main_plantilla.blade.php
@@ -6,9 +6,14 @@
 </head>
 <body>
 	 
-   
-
-    <script type="text/javascript">  
+		{{-- menu	 --}}
+		@include('common.menu')
+		{{-- contenido --}}
+		@yield('content')
+		
+		<!--Div where the WhatsApp will be rendered-->
+		<div id="WAButton" style="border-radius: 50%; z-index:1000"></div>
+		<script type="text/javascript">  
 		$(function() {
 		 $('#WAButton').floatingWhatsApp({
 		   phone: '1231231231', //WhatsApp Business phone number International format-
@@ -18,22 +23,14 @@
 		   showPopup: true, //Enables popup display
 		//    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
 		   headerColor: '#8ED825', //Custom header color
-		    // backgroundColor: 'crimson', //Custom background button color
+		   // backgroundColor: 'crimson', //Custom background button color
 		   position: "right"    
 		 });
-	   });
-	   </script>  
-
-	{{-- menu	 --}}
-	@include('common.menu')
-	{{-- contenido --}}
-	@yield('content')
-
-	 <!--Div where the WhatsApp will be rendered-->
-	 <div id="WAButton" style="border-radius: 50%; z-index:1000"></div>
-
-	{{-- footer --}}
-	@include('common.plantilla_footer')
-
+		});
+		</script>  
+	 
+	 	{{-- footer --}}
+		 @include('common.plantilla_footer')
+	 
 </body>
 </html>
