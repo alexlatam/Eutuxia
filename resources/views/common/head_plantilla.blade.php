3cf8cb0ad39a81a116c1d0970033891081205761
Carlos Maita <carlosmaita2009@gmail.com>
2020-09-07 01:00:57 -0400

    ajustes
    Ajustes de Ws
    Ajustes de vsta responsive

--- resources/views/common/head_plantilla.blade.php
+++ resources/views/common/head_plantilla.blade.php
@@ -6,6 +6,16 @@
 
 <link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/style.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/vendor.css')}}">
+<!--Floating WhatsApp css-->
+<link rel="stylesheet" href="{{asset('css/floating-wpp.css')}}" />
 
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+<!--Jquery-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+{{-- <script src="{{asset('js/floating-wpp.js')}}"></script> --}}
+
+<!-- metas -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
