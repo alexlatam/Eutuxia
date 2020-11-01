096db2d63650022c68ee6c79a13b9f25164f79ff
Carlos Maita <carlosmaita2009@gmail.com>
2020-11-01 01:56:16 -0400

    j

--- resources/views/common/plantilla_footer.blade.php
+++ resources/views/common/plantilla_footer.blade.php
@@ -65,7 +65,6 @@
 <link rel="stylesheet" href="{{asset('css/floating-wpp.css')}}"/>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-<link href="{{asset('vendor/aos/aos.css')}}">
 <link href="{{asset('vendor/aos/aos.css')}}" type="text/css" rel="stylesheet"/>
 
 
