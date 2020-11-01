42afffe7036aed6623b33b41a377e840763ee6d4
Carlos Maita <carlosmaita2009@gmail.com>
2020-11-01 11:32:15 -0400

    i

--- resources/views/common/head_plantilla.blade.php
+++ resources/views/common/head_plantilla.blade.php
@@ -23,9 +23,13 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<link rel="preload" as="style" href="{{asset('css/plantilla_css/style.css')}}">
 <link rel="preload" as="style" type="text/css" href="{{asset('css/plantilla_css/vendor.css')}}">
 <link rel="preload" as="style" type="text/css" href="{{asset('css/estilos_images_dinamicas.css')}}">
 <link rel="preload" as="style" href="{{asset('css/floating-wpp.css')}}"/>
+<link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Rubik:300,400,700">
+<link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,500,700">
+<link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap">
 {{-- <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" > --}}
 <link rel="preload" as="style" href="{{asset('vendor/aos/aos.css')}}" type="text/css" />
@@ -35,3 +39,4 @@
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{asset('vendor/animejs/anime.min.js')}}"></script>
 
+https://fonts.googleapis.com/css?family=Rubik:300,400,700
