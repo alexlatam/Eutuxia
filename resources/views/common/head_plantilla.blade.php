f7002c989901d9f13c34bc46c83f21d1d8f0d206
Carlos Maita <carlosmaita2009@gmail.com>
2020-11-01 01:50:13 -0400

    e

--- resources/views/common/head_plantilla.blade.php
+++ resources/views/common/head_plantilla.blade.php
@@ -27,5 +27,5 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/style.css')}}">
 <!--Jquery-->
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script defer  src="{{asset('vendor/animejs/anime.min.js')}}"></script>
+<script src="{{asset('vendor/animejs/anime.min.js')}}"></script>
 
