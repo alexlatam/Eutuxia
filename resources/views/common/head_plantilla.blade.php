b206acc0565c023a2561f8134d0fd83820f6407b
Carlos Maita <carlosmaita2009@gmail.com>
2020-11-01 12:30:59 -0400

    y

--- resources/views/common/head_plantilla.blade.php
+++ resources/views/common/head_plantilla.blade.php
@@ -22,9 +22,11 @@
 <!-- metas -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-{{-- precargas css  --}}
+{{-- precargas main --}}
 <link rel="preload" as="style" href="{{asset('css/plantilla_css/style.css')}}">
-<link rel="preload" as="style" type="text/css" href="{{asset('css/plantilla_css/vendor.css')}}">
+<link rel="preload" as="style" href="{{asset('css/plantilla_css/vendor.css')}}">
+<link rel="preload" as="script" href="{{asset('js/plantilla_js/vendor.js')}}" >
+{{-- precargas css  --}}
 <link rel="preload" as="style" type="text/css" href="{{asset('css/estilos_images_dinamicas.css')}}">
 <link rel="preload" as="style" href="{{asset('css/floating-wpp.css')}}"/>
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Rubik:300,400,700">
@@ -33,15 +35,15 @@
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap">
 <link rel="preload" as="style" href="{{asset('vendor/aos/aos.css')}}" type="text/css" />
 {{-- precargas js  --}}
-<link rel="preload" href="{{asset('vendor/animejs/anime.min.js')}}" as="script" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" as="script"  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+<link rel="preload" href="{{asset('vendor/animejs/anime.min.js')}}" as="script">
 <link rel="preload" href="{{asset('js/plantilla_js/app.js')}}" as="script">
-<link rel="preload" href="{{asset('js/plantilla_js/vendor.js')}}" as="script">
 <link rel="preload" href="{{asset('vendor/aos/aos.js')}}" as="script">
 
 {{-- main css --}}
 <link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/style.css')}}">
 
 {{-- main js --}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="{{asset('js/plantilla_js/app.js')}}"></script>
 <script src="{{asset('vendor/animejs/anime.min.js')}}"></script>
