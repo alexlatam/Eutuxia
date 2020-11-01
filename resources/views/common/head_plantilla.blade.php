0134bc4ebca7d135bae10b438acab914b27bb6e2
Carlos Maita <carlosmaita2009@gmail.com>
2020-11-01 01:42:15 -0400

    a

--- resources/views/common/head_plantilla.blade.php
+++ resources/views/common/head_plantilla.blade.php
@@ -22,21 +22,22 @@
 <!-- metas -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+{{-- Precargas --}}
+<link rel="preload" type="text/css" href="{{asset('css/estilos_images_dinamicas.css')}}" as="styles">
+<link rel="preload" href="{{asset('css/floating-wpp.css')}}" as="styles"/>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="preload"  as="styles">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="preload"  as="styles">
+<link rel="preload" href="{{asset('vendor/aos/aos.css')}}"   as="styles">
 
 
-<link rel="stylesheet" type="text/css" href="{{asset('css/estilos_images_dinamicas.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/style.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/vendor.css')}}">
-<!--Floating WhatsApp css-->
-<link rel="stylesheet" href="{{asset('css/floating-wpp.css')}}"/>
 
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-{{-- <script src="{{asset('js/floating-wpp.js')}}"></script> --}}
-<link href="{{asset('vendor/aos/aos.css')}}">
-<!--AOS-->
-<link href="{{asset('vendor/aos/aos.css')}}" type="text/css" rel="stylesheet"/>
 <!--Jquery-->
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script defer  src="{{asset('vendor/animejs/anime.min.js')}}"></script>
+
+{{-- <script src="{{asset('js/floating-wpp.js')}}"></script> --}}
+
+<link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/style.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/plantilla_css/vendor.css')}}">
+
 
